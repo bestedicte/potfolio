9cbac2c49497775653743fd3f9980325ff38dac7
benedicte vikvam <benedictevikvam@gmail.com>
2025-01-21 13:29:40 +0100

    Add dark mode to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,59 @@
-import Name from "../Name";
+'use client'
+
+import * as React from 'react'
+import { useTheme } from 'next-themes'
+import Name from '../Name'
 import {
     NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
-    NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu"
-import React from "react";
-
+    navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
+import { Moon, Sun } from 'lucide-react'
 
 export const Header: React.FC = () => {
+    const { setTheme } = useTheme()
+
     return (
-        <header className="flex bg-teal-50">
-            <Name/>
+        <header className="flex bg-background gap-12">
+            <Name />
             <NavigationMenu>
                 <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <NavigationMenuLink>Link</NavigationMenuLink>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
+                    <NavigationMenuLink
+                        href={'/projects'}
+                        className={navigationMenuTriggerStyle()}
+                    >
+                        Test
+                    </NavigationMenuLink>
                 </NavigationMenuList>
             </NavigationMenu>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="icon">
+                        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                        <span className="sr-only">Toggle theme</span>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuItem onClick={() => setTheme('light')}>
+                        Light
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme('dark')}>
+                        Dark
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme('system')}>
+                        System
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </header>
     )
 }
